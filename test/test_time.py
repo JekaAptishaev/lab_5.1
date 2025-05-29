09b16db645d081e94c971ac1865b500f928ed193
Djeka <jelyalaga@gmail.com>
2025-05-29 23:21:29 +0000

    финальная версия теста для 1

--- test/test_time.py
+++ test/test_time.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from app_v1 import app
+from app import app
 
 def test_time_route():
     with app.test_client() as client:

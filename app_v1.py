8c41ff3c19443266d2bf2b2a507607aea29ce860
Djeka <jelyalaga@gmail.com>
2025-05-29 22:52:48 +0000

    новая версия приложения

--- app_v1.py
+++ app_v1.py
@@ -3,9 +3,6 @@ import time
 
 app = Flask(__name__)
 
-@app.route("/time")
+@app.route('/time')
 def get_time():
     return jsonify({"time": int(time.time())})
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3030)
